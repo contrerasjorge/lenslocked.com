e3982e273216ff6f6ccb2bf979dc32bb15f3eaa8
Jorge Contreras <jorge.contreras789@gmail.com>
2020-01-06 18:05:18 -0500

    Created a sample main.go file

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+
+	"github.com/gorilla/mux"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch , please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
+
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path == "/" {
+		
+// 	} else if r.URL.Path == "/contact" {
+		
+// 	} else {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprint(w, "<h1>We could not find the page you "+
+// 		"were looking for :(</h1>" +
+// 		"<p>Please email us if you keep being sent to an "+
+// 		"invalid page.</p>")
+// 	}
+// }
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
