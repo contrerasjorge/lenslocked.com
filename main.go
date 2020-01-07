e1cf0b3a4adfb53af8b5dd9842fbb82279595d61
Jorge Contreras <jorge.contreras789@gmail.com>
2020-01-07 14:47:05 -0500

    Cleaned main.go file

--- main.go
+++ main.go
@@ -14,24 +14,10 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch , please send an email "+
+	fmt.Fprint(w, "To get in touch, please send an email "+
 		"to <a href=\"mailto:support@lenslocked.com\">"+
 		"support@lenslocked.com</a>.")
 }
-
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-// 	if r.URL.Path == "/" {
-		
-// 	} else if r.URL.Path == "/contact" {
-		
-// 	} else {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "<h1>We could not find the page you "+
-// 		"were looking for :(</h1>" +
-// 		"<p>Please email us if you keep being sent to an "+
-// 		"invalid page.</p>")
-// 	}
-// }
 
 func main() {
 	r := mux.NewRouter()
