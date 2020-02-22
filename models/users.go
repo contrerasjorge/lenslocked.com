8c46bf2287f9a04461e676b1be658b63ea3fb649
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-22 12:56:57 -0500

    Finished connecting the signup form with the user model

--- models/users.go
+++ models/users.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 
 	"github.com/jinzhu/gorm"
+	// Using gorm's postgres dialect
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -69,6 +72,12 @@ func first(db *gorm.DB, dst interface{}) error {
 // Create will create the provided user and backfill data
 // like the ID, CreatedAt and UpdatedAt fields
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
@@ -111,6 +120,8 @@ func (us *UserService) AutoMigrate() error {
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
