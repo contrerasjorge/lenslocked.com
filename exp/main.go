8d7b5047a2999adb6e19a67d3b7a2994be286ffc
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-25 13:53:44 -0500

    Created an HMAC type that allows us to hash strings easily

--- exp/main.go
+++ exp/main.go
@@ -1,51 +1,21 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 
-	_ "github.com/lib/pq"
-	"lenslocked.com/models"
-)
-
-const (
-	host   = "localhost"
-	port   = 5432
-	user   = "contrerasjorge"
-	dbname = "lenslocked_dev"
+	"lenslocked.com/hash"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-		host, port, user, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer us.Close()
+	toHash := []byte("this is my string to hash")
+	h := hmac.New(sha256.New, []byte("my-secret-key"))
+	h.Write(toHash)
+	b := h.Sum(nil)
+	fmt.Println(base64.URLEncoding.EncodeToString(b))
 
-	us.DestructiveReset()
-
-	user := models.User{
-		Name:  "Jimmy Jim",
-		Email: "abc@abc.io",
-	}
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	// user.Email = "abc@cde.io"
-	// if err := us.Update(&user); err != nil {
-	// 	panic(err)
-	// }
-
-	// UserByEmail, err := us.ByEmail("abc@cde.io")
-
-	if err := us.Delete(user.ID); err != nil {
-		panic(err)
-	}
-	userByID, err := us.ByID(user.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(userByID)
+	hmac := hash.NewHMAC("my-secret-key")
+	fmt.Println(hmac.Hash("this is my string to hash"))
 }
