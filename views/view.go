32481b272ab649ac8911953bf8702223eb65258e
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-29 15:21:38 -0500

    Implemented error handling for render errors

--- views/view.go
+++ views/view.go
@@ -1,7 +1,9 @@
 package views
 
 import (
+	"bytes"
 	"html/template"
+	"io"
 	"net/http"
 	"path/filepath"
 )
@@ -35,7 +37,7 @@ func layoutFiles() []string {
 	return files
 }
 
-func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+func (v *View) Render(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html")
 	switch data.(type) {
 	case Data:
@@ -46,7 +48,12 @@ func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 		}
 	}
 
-	return v.Template.ExecuteTemplate(w, v.Layout, data)
+	var buf bytes.Buffer
+	if err := v.Template.ExecuteTemplate(&buf, v.Layout, data); err != nil {
+		http.Error(w, "Something went wrong. If the problem persists please email support@lenslocked.com", http.StatusInternalServerError)
+		return
+	}
+	io.Copy(w, &buf)
 }
 
 type View struct {
@@ -55,9 +62,7 @@ type View struct {
 }
 
 func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := v.Render(w, nil); err != nil {
-		panic(err)
-	}
+	v.Render(w, nil)
 }
 
 // addTemplatePath takes in a slice of strings
