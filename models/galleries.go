38b1b9b829e4deac99516a0eef7913308fa84f56
Jorge Contreras <jorge.contreras789@gmail.com>
2020-03-03 13:37:31 -0500

    Implemented the gallery service and set it on the Service type

--- models/galleries.go
+++ models/galleries.go
@@ -18,10 +18,28 @@ type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
 
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{
+			GalleryDB: &galleryGorm{
+				db: db},
+		}}
+}
+
+type galleryService struct {
+	GalleryDB
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+var _ GalleryDB = &galleryGorm{}
+
 type galleryGorm struct {
 	db *gorm.DB
 }
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
-	return nil
+	return gg.db.Create(gallery).Error
 }
